84e5776d11fac67aed32baf432299aa29cc89a2e
Sandy Obare <obaresandy@gmail.com>
2025-02-01 12:57:54 +0300

    Updating data source parameters to sync with postgresql database

--- backend/src/data-source.ts
+++ backend/src/data-source.ts
@@ -1,17 +1,19 @@
 import "reflect-metadata"
 import { DataSource } from "typeorm"
 import { User } from "./entity/User"
+import { Enrollments } from "./entity/Enrollment"
+import { Campaign } from "./entity/Campaign"
 
 export const AppDataSource = new DataSource({
-    type: "mysql",
+    type: "postgres",
     host: "localhost",
-    port: 3306,
+    port: 5432,
     username: "cc_dev",
     password: "CC_dev_pwd1",
     database: "cc_dev_db",
     synchronize: true,
     logging: false,
-    entities: [User],
+    entities: [User, Enrollments, Campaign],
     migrations: [],
     subscribers: [],
 })
